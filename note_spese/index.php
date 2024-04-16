337481a8561939a73b214367a15362d4e5b1a96d
aakash <123299191+tingitingi@users.noreply.github.com>
2024-04-16 21:55:41 +0200

    Add files via upload

--- note_spese/index.php
+++ note_spese/index.php
@@ -4,13 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Benvenuto</title>
+    <link rel="stylesheet" href="index.css">
 </head>
 <body>
     <div class="container">
-        <h1>Benvenuto!</h1>
-        <p>Effettua l'accesso o registrati per accedere al nostro servizio.</p>
-        <a href="login.php" class="button">Login</a>
-        <a href="registrazione.php" class="button">Registrati</a>
+        <div class="title">
+            <p class="logo">Speshub</p>
+        </div>
+        <div class="button-container">
+            <a href="registrazione.php" class="button">Registrati</a>
+            <a href="login.php" class="button">Accedi</a>
+        </div>
+        <hr class="separatore"> <!-- Linea orizzontale con classe "separatore" -->
+        <div class="content">
+            <h1 class="left-align">Benvenuto su SpesHub: La tua soluzione per la Gestione delle Note Spese</h1>
+            <p class="left-align">Benvenuti su SpesHub, il tuo nuovo compagno affidabile per semplificare la gestione delle note spese. Siamo qui per liberarvi dal fastidio e dalla frustrazione legati alla compilazione e alla gestione delle note spese, offrendovi un'esperienza intuitiva e senza stress.</p>
+        </div>
     </div>
 </body>
 </html>
