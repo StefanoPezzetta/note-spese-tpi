62ceed57eff4b4afea304a3bbd5586c76d8d78e6
aakash <123299191+tingitingi@users.noreply.github.com>
2024-05-24 22:29:06 +0200

    Delete getSessionData.php

--- getSessionData.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-$sessionData = [];
-$sessionData['notes'] = $_SESSION['notes']; 
-$sessionData['user'] = $_SESSION['datiUtente'];
-
-header('Content-Type: application/json');
-echo json_encode($sessionData);
